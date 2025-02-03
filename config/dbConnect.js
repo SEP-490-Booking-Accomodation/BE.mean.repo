4a435284de457d4a230413c881b3d39d38187c9e
Nguyên Nguyễn <124855591+NNguyen1202@users.noreply.github.com>
2025-02-03 13:52:36 +0700

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,8 +2,8 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = () => {
   try {
-    //const conn = mongoose.connect(process.env.MONGODB_URL);
-    const conn = mongoose.connect(process.env.MONGODB_URL_LOCAL);
+    const conn = mongoose.connect(process.env.MONGODB_URL);
+    //const conn = mongoose.connect(process.env.MONGODB_URL_LOCAL);
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Database error");
