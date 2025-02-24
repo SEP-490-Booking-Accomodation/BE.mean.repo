026f0a70fefde4f410d879d19d0d6a24e09500d4
Nguyên Nguyễn <124855591+NNguyen1202@users.noreply.github.com>
2025-02-24 21:12:00 +0700

    Update

--- models/staffModel.js
+++ models/staffModel.js
@@ -2,45 +2,45 @@ const mongoose = require("mongoose"); // Erase if already required
 const moment = require("moment-timezone");
 
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema(
-    {
-        userId: {
-            type: mongoose.Schema.ObjectId,
-            ref: "User",
-        },
-        isDelete: {
-            type: Boolean,
-            default: false,
-        },
+var staffSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
     },
-    {
-        timestamps: true,
-        toJSON: {
-            virtuals: true,
-            transform: (doc, ret) => {
-                ret.createdAt = moment(ret.createdAt)
-                    .tz("Asia/Ho_Chi_Minh")
-                    .format("DD/MM/YYYY HH:mm:ss");
-                ret.updatedAt = moment(ret.updatedAt)
-                    .tz("Asia/Ho_Chi_Minh")
-                    .format("DD/MM/YYYY HH:mm:ss");
-                return ret;
-            },
-        },
-        toObject: {
-            virtuals: true,
-            transform: (doc, ret) => {
-                ret.createdAt = moment(ret.createdAt)
-                    .tz("Asia/Ho_Chi_Minh")
-                    .format("DD/MM/YYYY HH:mm:ss");
-                ret.updatedAt = moment(ret.updatedAt)
-                    .tz("Asia/Ho_Chi_Minh")
-                    .format("DD/MM/YYYY HH:mm:ss");
-                return ret;
-            },
-        },
-    }
+    isDelete: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.createdAt = moment(ret.createdAt)
+          .tz("Asia/Ho_Chi_Minh")
+          .format("DD/MM/YYYY HH:mm:ss");
+        ret.updatedAt = moment(ret.updatedAt)
+          .tz("Asia/Ho_Chi_Minh")
+          .format("DD/MM/YYYY HH:mm:ss");
+        return ret;
+      },
+    },
+    toObject: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.createdAt = moment(ret.createdAt)
+          .tz("Asia/Ho_Chi_Minh")
+          .format("DD/MM/YYYY HH:mm:ss");
+        ret.updatedAt = moment(ret.updatedAt)
+          .tz("Asia/Ho_Chi_Minh")
+          .format("DD/MM/YYYY HH:mm:ss");
+        return ret;
+      },
+    },
+  }
 );
 
 //Export the model
-module.exports = mongoose.model("Staff", userSchema);
+module.exports = mongoose.model("Staff", staffSchema);
