dc7373852ce193e7995a2875e7c56b237308e90a
Nguyên Nguyễn <124855591+NNguyen1202@users.noreply.github.com>
2025-03-03 22:03:04 +0700

    Update firebase config

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../mean-dfd43-firebase-adminsdk-fbsvc-fbed992596.json"); // Thay đường dẫn tệp JSON
+const serviceAccount = require("../config/mean-dfd43-firebase-adminsdk-fbsvc-6bf532f724.json"); // Thay đường dẫn tệp JSON
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
