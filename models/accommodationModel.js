43ccedcf668f225804aefab1fe72c92914f20725
Nguyên Nguyễn <124855591+NNguyen1202@users.noreply.github.com>
2025-04-10 16:15:57 +0700

    Update

--- models/accommodationModel.js
+++ models/accommodationModel.js
@@ -1,72 +1,71 @@
 const mongoose = require("mongoose"); // Erase if already required
 const moment = require("moment-timezone");
 const ACCOMMODATION_STATUS = Object.freeze({
-    AVAILABLE: 1,
-    BOOKED: 2,
-    CLEANING: 3,
-    PREPARING: 4,
-    MAINTENANCE: 5,
-    CLOSED: 6,
-    INUSE: 7
+  AVAILABLE: 1,
+  BOOKED: 2,
+  CLEANING: 3,
+  PREPARING: 4,
+  MAINTENANCE: 5,
+  CLOSED: 6,
+  INUSE: 7,
 });
 var accommodationSchema = new mongoose.Schema(
-    {
-        rentalLocationId: {
-            type: mongoose.Schema.ObjectId,
-            ref: "RentalLocation",
-        },
-        accommodationTypeId: {
-            type: mongoose.Schema.ObjectId,
-            ref: "AccommodationType",
-        },
-        roomNo:{
-            type: String,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        image: [
-            {
-                type: String,
-            },
-        ],
-        status: {
-            type: Number,
-            enum: Object.values(ACCOMMODATION_STATUS),
-        },
-        isDelete: {
-            type: Boolean,
-            default: false,
-        },
+  {
+    rentalLocationId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "RentalLocation",
     },
-    {
-        timestamps: true,
-        toJSON: {
-            virtuals: true,
-            transform: (doc, ret) => {
-                ret.createdAt = moment(ret.createdAt)
-                    .tz("Asia/Ho_Chi_Minh")
-                    .format("DD/MM/YYYY HH:mm:ss");
-                ret.updatedAt = moment(ret.updatedAt)
-                    .tz("Asia/Ho_Chi_Minh")
-                    .format("DD/MM/YYYY HH:mm:ss");
-                return ret;
-            },
-        },
-        toObject: {
-            virtuals: true,
-            transform: (doc, ret) => {
-                ret.createdAt = moment(ret.createdAt)
-                    .tz("Asia/Ho_Chi_Minh")
-                    .format("DD/MM/YYYY HH:mm:ss");
-                ret.updatedAt = moment(ret.updatedAt)
-                    .tz("Asia/Ho_Chi_Minh")
-                    .format("DD/MM/YYYY HH:mm:ss");
-                return ret;
-            },
-        },
-    }
+    accommodationTypeId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "AccommodationType",
+    },
+    roomNo: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    image: [
+      {
+        type: String,
+      },
+    ],
+    status: {
+      type: Number,
+      enum: Object.values(ACCOMMODATION_STATUS),
+    },
+    isDelete: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.createdAt = moment(ret.createdAt)
+          .tz("Asia/Ho_Chi_Minh")
+          .format("DD/MM/YYYY HH:mm:ss");
+        ret.updatedAt = moment(ret.updatedAt)
+          .tz("Asia/Ho_Chi_Minh")
+          .format("DD/MM/YYYY HH:mm:ss");
+        return ret;
+      },
+    },
+    toObject: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.createdAt = moment(ret.createdAt)
+          .tz("Asia/Ho_Chi_Minh")
+          .format("DD/MM/YYYY HH:mm:ss");
+        ret.updatedAt = moment(ret.updatedAt)
+          .tz("Asia/Ho_Chi_Minh")
+          .format("DD/MM/YYYY HH:mm:ss");
+        return ret;
+      },
+    },
+  }
 );
 //Export the model
 module.exports = mongoose.model("Accommodation", accommodationSchema);
